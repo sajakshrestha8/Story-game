57db4b26649f7741eb8f762bcace980b087bb223
sajakshrestha8 <shresthasajak123@gmail.com>
2026-01-25 09:24:32 +0545

    enhanced the floor

--- objects/floor.js
+++ objects/floor.js
@@ -1,9 +1,21 @@
 export class Floor {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, speed = 0) {
     this.height = height;
     this.width = width;
     this.x = x;
     this.y = y;
+    this.speed = speed;
+    this.direction = 1;
+  }
+
+  update(deltaTime) {
+    if (this.speed === 0) return;
+
+    this.x += this.speed * this.direction * deltaTime;
+
+    if (this.x < 0 || this.x + this.width > 1000) {
+      this.direction *= -1;
+    }
   }
 
   draw(ctx) {
