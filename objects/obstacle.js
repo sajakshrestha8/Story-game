dd0e8e364f68910a6d7a389f31d37b99ca461bd8
sajakshrestha8 <shresthasajak123@gmail.com>
2026-01-31 15:10:48 +0545

    hide the switch and obstacles

--- objects/obstacle.js
+++ objects/obstacle.js
@@ -6,6 +6,7 @@ export default class Obstacle {
     this.endAngle = endAngle;
     this.radius = radius;
     this.enabled = enabled;
+    this.isVisible = false;
   }
 
   createObstacle(enabled) {
@@ -18,6 +19,7 @@ export default class Obstacle {
   }
 
   moveObstacle(deltaTime, direction, speed) {
+    this.isVisible = true;
     if (direction === "left") {
       this.x = this.x - speed * deltaTime;
     } else {
@@ -31,6 +33,7 @@ export default class Obstacle {
   }
 
   draw(ctx) {
+    if (!this.isVisible) return;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
     ctx.fillStyle = "red";
--- objects/switch.js
+++ objects/switch.js
@@ -5,6 +5,7 @@ export default class Switch {
     this.width = width;
     this.height = height;
     this.isOn = false;
+    this.isVisible = false;
   }
 
   toggle() {
@@ -13,6 +14,11 @@ export default class Switch {
 
   draw(ctx) {
     ctx.save();
+
+    // Curently the switch is invisible
+    if (!this.isVisible) {
+      return;
+    }
 
     // Switch background
     ctx.fillStyle = this.isOn ? "#4caf50" : "#aaa";
